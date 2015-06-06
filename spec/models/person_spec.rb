cc7e67d32746b7d869a5e10bce8726b56280ba1d
Josh Cass <joshuacass@gmail.com>
2015-06-06 13:52:04 -0600

    validations on person

--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -2,12 +2,12 @@ require 'rails_helper'
 
 RSpec.describe Person, type: :model do
   it 'is invalid without a first name' do
-    person = Person.new(first_name: nil)
+    person = Person.new(first_name: nil, last_name: "Solo")
     expect(person).not_to be_valid
   end
 
   it 'is invalid without a last name' do
-    person = Person.new(first_name: "Bob", last_name: nil)
+    person = Person.new(first_name: "Han", last_name: nil)
     expect(person).not_to be_valid
   end
 end
